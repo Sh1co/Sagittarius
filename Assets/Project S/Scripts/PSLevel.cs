3a6d277f9f2b415bf45683aaf1e38d61d8f47776
Sherif Nafee <shico.m.nafee@gmail.com>
2022-10-11 15:06:30 +0300

    Add PSLevel

---
+++ Assets/Project S/Scripts/PSLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PSLevel : MonoBehaviour
+{
+    [SerializeField] private String _sceneName;
+    [SerializeField] private String _mainMenuSceneName;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Hamada");
+        SceneManager.LoadScene(_mainMenuSceneName);
+    }
+    
+    
+}
