fb9b009b541f989212f5946ec8fef597ab485c18
Sh1co <shico.m.nafee@gmail.com>
2022-11-07 17:07:59 +0000

    Automated changes

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -15,10 +15,6 @@ func _ready():
 	pass  # Replace with function body.
 
 
-
-
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_up"):
