6b60232e2ee9ef02debab26012c265551d9d15c8
Sherif Nafee <shico.m.nafee@gmail.com>
2022-11-08 18:39:20 +0300

    Add health bar management to level

--- Scripts/SLevel.gd
+++ Scripts/SLevel.gd
@@ -3,14 +3,23 @@ extends Node2D
 signal level_complete
 
 export(PackedScene) var s_player
+export(PackedScene) var s_healthbar
+
 var player
+var healthbar
 
 
 func _ready():
+	# Add Player to level
 	player = s_player.instance()
 	player.position = $StartPosition.position
 	player.connect("player_died", self, "player_died")
 	add_child(player)
+
+	# Add healthbar to level
+	healthbar = s_healthbar.instance()
+	healthbar.init(player.health, player.health)
+	add_child(healthbar)
 
 
 func level_complete():
@@ -18,13 +27,16 @@ func level_complete():
 	emit_signal("level_complete")
 	self.queue_free()
 
+
 func player_died():
 	print("Player died, reseting level.")
 	get_tree().reload_current_scene()
 
+
 func change_player_health(change):
 	print("Player health changed by " + str(change))
 	player.change_health(change)
+	healthbar.health_changed(player.health)
 
 
 func _on_Button_pressed():
