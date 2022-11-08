04261bdadb289fd8f446d7972402a7ada26730a9
Sherif Nafee <shico.m.nafee@gmail.com>
2022-11-08 18:04:09 +0300

    Make level handle player health

--- Scripts/SLevel.gd
+++ Scripts/SLevel.gd
@@ -3,11 +3,13 @@ extends Node2D
 signal level_complete
 
 export(PackedScene) var s_player
+var player
 
 
 func _ready():
-	var player = s_player.instance()
+	player = s_player.instance()
 	player.position = $StartPosition.position
+	player.connect("player_died", self, "player_died")
 	add_child(player)
 
 
@@ -16,6 +18,18 @@ func level_complete():
 	emit_signal("level_complete")
 	self.queue_free()
 
+func player_died():
+	print("Player died, reseting level.")
+	get_tree().reload_current_scene()
+
+func change_player_health(change):
+	print("Player health changed by " + str(change))
+	player.change_health(change)
+
 
 func _on_Button_pressed():
 	level_complete()
+
+
+func _on_Button2_pressed():
+	change_player_health(-20)
