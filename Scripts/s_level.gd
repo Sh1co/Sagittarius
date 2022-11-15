3c5c86e4aab0f2399af334916692433451f3a838
Sherif Nafee <shico.m.nafee@gmail.com>
2022-11-15 09:10:37 +0300

    Make SLevel more abstract

--- Scripts/s_level.gd
+++ Scripts/s_level.gd
@@ -3,66 +3,11 @@ extends Node2D
 
 signal level_complete
 
-export(PackedScene) var s_player
-export(PackedScene) var s_healthbar
-export(PackedScene) var s_enemy_spawner
-
-var player
-var healthbar
-
-
-func _ready():
-	_spawn_player()
-
-	_add_health_bar()
-
-	_spawn_enemies()
-
-
 func level_complete():
 	print("Level Complete!")
 	emit_signal("level_complete")
 	self.queue_free()
-
-
+	
 func player_died():
 	print("Player died, reseting level.")
 	get_tree().reload_current_scene()
-
-
-func change_player_health(change):
-	print("Player health changed by " + str(change))
-	player.change_health(change)
-	healthbar.health_changed(player.health)
-
-
-func _on_Button_pressed():
-	level_complete()
-
-
-func _on_Button2_pressed():
-	change_player_health(-20)
-
-
-func _spawn_enemies():
-	var enemy_spawner = s_enemy_spawner.instance() as SEnemySpawner
-	add_child(enemy_spawner)
-	enemy_spawner.connect("enemy_spawned", self, "_on_enemy_spawned")
-	enemy_spawner.spawn()
-
-
-func _on_enemy_spawned(enemy):
-	enemy.connect("attacked_player", self, "change_player_health")
-
-
-func _spawn_player():
-	player = s_player.instance() as SPlayer
-	player.position = $StartPosition.position
-	player.connect("player_died", self, "player_died")
-	add_child(player)
-
-
-func _add_health_bar():
-	healthbar = s_healthbar.instance() as SHealthBar
-	healthbar.init(player.health, player.health)
-	add_child(healthbar)
--- Scripts/s_main_menu.gd
+++ Scripts/s_main_menu.gd
@@ -1,18 +1,5 @@
 class_name SMainMenu
 extends SLevel
 
-
-func _spawn_player():
-	pass
-
-
-func _add_health_bar():
-	pass
-
-
-func _spawn_enemies():
-	pass
-
-
 func _on_Start_pressed():
 	level_complete()
