0c7bd24a40da53f507761144e98cd98533477bc8
Sherif Nafee <shico.m.nafee@gmail.com>
2022-11-15 09:35:24 +0300

    Use the built-in damping for player movement

--- Scripts/s_player.gd
+++ Scripts/s_player.gd
@@ -7,7 +7,7 @@ signal player_died
 # var a = 2
 # var b = "text"
 export var health = 100
-export var top_speed = Vector2(250, 250)
+export var top_speed = Vector2(350, 350)
 export var acceleration = Vector2(450, 450)
 export var deceleration = Vector2(600, 600)
 
@@ -26,28 +26,31 @@ func _process(delta):
 
 
 func _movement(delta):
+	velocity = linear_velocity
+	
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= acceleration.y * delta
 		if velocity.y > 0:
 			velocity.y -= deceleration.y * delta
+		_update_velocity()
 	elif Input.is_action_pressed("move_down"):
 		velocity.y += acceleration.y * delta
 		if velocity.y < 0:
 			velocity.y += deceleration.y * delta
-	else:
-		velocity.y += deceleration.x * delta * (1 if velocity.y < 0 else -1)
+		_update_velocity()
 
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= acceleration.x * delta
 		if velocity.x > 0:
 			velocity.x -= deceleration.x * delta
+		_update_velocity()
 	elif Input.is_action_pressed("move_right"):
 		velocity.x += acceleration.x * delta
 		if velocity.x < 0:
 			velocity.x += deceleration.x * delta
-	else:
-		velocity.x += deceleration.x * delta * (1 if velocity.x < 0 else -1)
-
+		_update_velocity()
+	
+func _update_velocity():
 	velocity.x = clamp(velocity.x, -top_speed.x, top_speed.x)
 	velocity.y = clamp(velocity.y, -top_speed.y, top_speed.y)
 
