14c5adad3db91e99f65112c68ee0fcecbc071a80
Sherif Nafee <shico.m.nafee@gmail.com>
2023-01-26 11:32:14 +0300

    Fix issues with the base script name

--- Scripts/GBS/gbs.gd
+++ Scripts/GBS/gbs.gd
@@ -1,29 +1,35 @@
-class_name GBS
 extends Node
-
 
 onready var save_data
 
+
 func _ready():
-	save_data
+	pass
+
 
 func _getsave_data():
 	pass
-	
+
+
 func save():
 	pass
-	
+
+
 func set_var(key, value):
 	pass
+
 
 func get_var(key, default):
 	pass
 
+
 func has_var(key):
 	pass
-	
+
+
 func delete_var(key):
 	pass
-	
+
+
 func delete_all():
 	pass
