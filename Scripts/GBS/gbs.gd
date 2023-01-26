e24736f2e0cb799458a7e7d59b5db6cd7ba6fe25
Sherif Nafee <shico.m.nafee@gmail.com>
2023-01-26 11:29:37 +0300

    Create base gbs script

---
+++ Scripts/GBS/gbs.gd
@@ -0,0 +1,29 @@
+class_name GBS
+extends Node
+
+
+onready var save_data
+
+func _ready():
+	save_data
+
+func _getsave_data():
+	pass
+	
+func save():
+	pass
+	
+func set_var(key, value):
+	pass
+
+func get_var(key, default):
+	pass
+
+func has_var(key):
+	pass
+	
+func delete_var(key):
+	pass
+	
+func delete_all():
+	pass
