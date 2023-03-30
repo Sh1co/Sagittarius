d5cb545274891f843512f3df9d83072407fdb565
Sherif Nafee <shico.m.nafee@gmail.com>
2023-03-30 14:43:18 +0300

    Add creation of level grid

---
+++ Scripts/s_level_selector.gd
@@ -0,0 +1,16 @@
+class_name SLevelSelector
+extends SLevel
+
+@export var levels: Array[String]
+@export var level_button: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_create_Level_Grid()
+
+
+func _create_Level_Grid():
+	for level in levels:
+		var lvl_btn = level_button.instantiate() as SLevelButton
+		$Control/LevelGrid.add_child(lvl_btn)
