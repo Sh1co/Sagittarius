fc7bfe84d635d765898eaf2520445790ad0ff46e
Sherif Nafee <shico.m.nafee@gmail.com>
2023-03-31 15:48:18 +0300

    Send level id to level buttons

--- Scripts/s_level_selector.gd
+++ Scripts/s_level_selector.gd
@@ -12,9 +12,10 @@ func _ready():
 
 
 func _create_Level_Grid():
-	for level in levels:
+	for ind in _levels.size():
 		var lvl_btn = level_button.instantiate() as SLevelButton
-		lvl_btn.init(level, 1)
+		lvl_btn.init(_levels[ind], ind)
+		lvl_btn.level_selected.connect(_on_level_selected.bind())
 		$Control/LevelGrid.add_child(lvl_btn)
 func _on_level_selected(id):
 	emit_signal("go_to_level",id)
