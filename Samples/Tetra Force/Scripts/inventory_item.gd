4132a93bb5efb57390af844471c4002b5334097e
Sherif Nafee <shico.m.nafee@gmail.com>
2023-06-08 18:44:18 +0300

    Fix formatting

--- Samples/Tetra Force/Scripts/inventory_item.gd
+++ Samples/Tetra Force/Scripts/inventory_item.gd
@@ -1,8 +1,6 @@
 extends Button
 
-
 signal equip_item(item)
-
 
 var item: PackedScene
 
@@ -12,17 +10,18 @@ func init(item_ps):
 	text = _fix_name(item.resource_path)
 
 
-func _fix_name(path:String):
+func _fix_name(path: String):
 	var f1 = false
 	var name = ""
-	for i in range(path.length()-1,-1,-1):
-		if path[i]=='/':
+	for i in range(path.length() - 1, -1, -1):
+		if path[i] == "/":
 			break
 		if f1:
 			name = path[i] + name
-		if path[i] == '.':
+		if path[i] == ".":
 			f1 = true
 	return name
+
 
 func _on_pressed():
 	emit_signal("equip_item", item)
