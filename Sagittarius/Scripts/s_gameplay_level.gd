60ef934513b747d1c11a0f7ab92353c483a33ff4
Sherif Nafee <shico.m.nafee@gmail.com>
2023-11-26 18:43:50 +0300

    Move everything into a folder
    Move everything into a Sagittarius folder to make it easy to start new projects

---
+++ Sagittarius/Scripts/s_gameplay_level.gd
@@ -0,0 +1,124 @@
+class_name SGamePlayLevel
+extends SLevel
+
+@export var s_player: PackedScene
+@export var player_start_pos: Node2D
+@export var s_healthbar: PackedScene
+@export var s_enemy_spawner: PackedScene
+@export var s_coins_spawner: PackedScene
+@export var s_coins_counter_ui: PackedScene
+@export var level_annoucement = false
+@export var level_title_scene: NodePath
+
+var player
+var healthbar
+var coins_counter_ui
+
+
+func _ready():
+	_spawn_player()
+
+	_add_health_bar()
+
+	_add_coins_counter()
+
+	_spawn_enemies()
+
+	_spawn_coins()
+
+	_announce_level()
+
+
+func change_player_health(change):
+	player.change_health(change)
+	print("Player health changed by " + str(change))
+
+
+func _on_Button_pressed():
+	complete_level()
+
+
+func _on_Button2_pressed():
+	change_player_health(-20)
+
+
+func _on_Button3_pressed():
+	print("Reseting Coins!")
+	if coins_manager != null:
+		coins_manager.change_coins(-coins_manager.get_coins())
+
+
+func _spawn_enemies():
+	if s_enemy_spawner == null:
+		return
+	var enemy_spawner = s_enemy_spawner.instantiate() as SEntitySpawner
+	add_child(enemy_spawner)
+	enemy_spawner.spawn()
+
+
+func _spawn_coins():
+	if s_coins_spawner == null:
+		return
+	var coins_spawner = s_coins_spawner.instantiate() as SEntitySpawner
+	add_child(coins_spawner)
+	coins_spawner.connect("entity_spawned", Callable(self, "_on_collectable_spawned"))
+	coins_spawner.spawn()
+
+
+func _on_collectable_spawned(collectable):
+	collectable.connect("collected", Callable(self, "_on_collectable_collected"))
+
+
+func _on_collectable_collected(value):
+	if coins_manager != null:
+		coins_manager.change_coins(value)
+		print("Coins collected! You now have: " + str(coins_manager.get_coins()))
+	else:
+		print("Coins collected! But coins manager isn't added to the game!")
+
+
+func _spawn_player():
+	if s_player == null:
+		return
+	player = s_player.instantiate() as SPlayer
+	player.position = player_start_pos.position
+	player.connect("player_died", Callable(self, "player_died"))
+	add_child(player)
+
+
+func _add_health_bar():
+	if s_healthbar == null:
+		return
+	healthbar = s_healthbar.instantiate() as SHealthBar
+#	healthbar.init(player.health, player.health)
+#	player.connect("health_changed", Callable(healthbar, "health_changed"))
+	$CanvasLayer.add_child(healthbar)
+
+
+func _add_coins_counter():
+	if s_coins_counter_ui == null:
+		return
+	coins_counter_ui = s_coins_counter_ui.instantiate() as SCoinsCounterUI
+	if coins_manager != null:
+		coins_manager.connect("coins_update", Callable(coins_counter_ui, "coins_updated"))
+		coins_counter_ui.coins_updated(coins_manager.get_coins())
+	else:
+		print("Coins manager was not added to game!")
+	$CanvasLayer.add_child(coins_counter_ui)
+
+
+func _announce_level():
+	var level_title = get_node(level_title_scene)
+	if level_title == null:
+		return
+	level_title.text = level_name
+	var tween = get_tree().create_tween()
+	if level_annoucement:
+		tween.tween_property(level_title, "modulate:a", 0, 0)
+		tween.tween_property(level_title, "modulate:a", 1, 1)
+		tween.tween_property(level_title, "modulate:a", 0, 1)
+	tween.tween_callback(level_title.queue_free)
+
+
+func _on_health_consumable_reset_pressed():
+	player.health_consumable.add(5)
